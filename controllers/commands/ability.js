5539d5a48027dac66d51ee3a6c704b4b97eefbde
Ryan Munro <Ryan.Munro@uts.edu.au>
2018-03-23 12:44:25 +1100

    Added additional fields and modified output, support for exact prioritsation

--- controllers/commands/ability.js
+++ controllers/commands/ability.js
@@ -1,25 +1,56 @@
 const mongoose = require('mongoose')
 const Ability = require('../models/ability')
 
+getAnnouncement = (announce) => {
+    switch(announce) {
+        case "Active": return "Announced on activation"
+        case "Enter": return "Announced on entry"
+        case "Hidden": return "Hidden"
+        default: return ""
+    }
+}
 exports.run = (client, message, args) => {
     if(args.length == 0) return
 
     var search = args.join(' ')
     Ability.find({ 'abilityName': new RegExp(search, 'i') }, (err, result) => {
-        if(result.length == 0) {
-            message.channel.send(`No results found for ${search}`)
-            return
+        switch (result.length) {
+            case 0:
+                message.channel.send(`No results found for ${search}`)
+                return
+            case 1:
+                result = result[0]
+                embed = { 
+                    title: `${result.abilityName}`,
+                    desc: `${result.desc}`, 
+                }
+                if(result.announcement) {
+                    embed.title += ` | ${getAnnouncement(result.announcement)}`
+                }
+                if(result.affects) {
+                    embed.fields.push({
+                        name: `**Interacts with the following:**`,
+                        value: `${result.affects}`
+                    })
+                }
+                if(result.additional) {
+                    embed.footer = {
+                        text: result.additional
+                    }
+                }
+                break
+            default:
+                embed = { 
+                    title: `${result.length} result(s) found for "${search}"`, 
+                    desc: ``, 
+                }
+                result.forEach((r) => {
+                    embed.desc += r.abilityName+`\n`
+                })
+                embed.footer = "For more information, search again with one of the listed abilities"
+                break
         }
-        embed = { title: `${result.length} result(s) found for "${search}"`, fields: [] }
-        result.forEach((r) => {
-            embed.fields.push({
-                name: `**${r.abilityName}**`,
-                value: `
-|${r.announcement}|
-${r.desc}
-Note: ${r.additional}`
-            })
-        })
+
         message.channel.send({'embed': embed} )
     })
 }
